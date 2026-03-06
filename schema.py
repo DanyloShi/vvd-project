8c65e3ee03284a0bc3eedd15ac7875d6de436623
Zimnov123 <sanazimnov52@gmail.com>
2026-03-06 13:47:18 +0200

    Fix display schema.py

--- schema.py
+++ schema.py
@@ -46,5 +46,45 @@ schema = StructType([
 spark = SparkSession.builder.appName("MusicData").getOrCreate()
 
 df = spark.read.csv("spotify_dataset.csv", header=True, schema=schema)
-df.show(5)         
-df.printSchema() 
+
+print("\nBasic song info")
+df.select(
+    "Artist(s)", "song", "Genre", "Album"
+).show(5, truncate=20)
+
+print("\nRelease info")
+df.select(
+    "Release Date", "Length", "emotion", "Popularity"
+).show(5, truncate=20)
+
+print("\nAudio specifications")
+df.select(
+    "Key", "Tempo", "Loudness (db)", "Time signature",
+    "Energy", "Danceability", "Positiveness"
+).show(5, truncate=20)
+
+print("\nMore audio metrics")
+df.select(
+    "Speechiness", "Liveness", "Acousticness", "Instrumentalness"
+).show(5, truncate=20)
+
+print("\nWhat activities is it suitable for")
+df.select(
+    "Good for Party",
+    "Good for Work/Study",
+    "Good for Relaxation/Meditation",
+    "Good for Exercise",
+    "Good for Running",
+    "Good for Yoga/Stretching",
+    "Good for Driving"
+).show(5)
+
+print("\nSimilar tracks")
+df.select(
+    "Similar Artist 1", "Similar Song 1", "Similarity Score 1",
+    "Similar Artist 2", "Similar Song 2", "Similarity Score 2",
+    "Similar Artist 3", "Similar Song 3", "Similarity Score 3"
+).show(5, truncate=20)
+
+print("\nSchema DataFrame")
+df.printSchema()
